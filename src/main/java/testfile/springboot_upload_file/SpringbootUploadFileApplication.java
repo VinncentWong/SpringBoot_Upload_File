a0cad3edf0b64420975c854c2924931ae3b2ea7c
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-06-09 20:54:17 +0700

    initial init

---
+++ src/main/java/testfile/springboot_upload_file/SpringbootUploadFileApplication.java
@@ -0,0 +1,13 @@
+package testfile.springboot_upload_file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootUploadFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootUploadFileApplication.class, args);
+	}
+
+}
